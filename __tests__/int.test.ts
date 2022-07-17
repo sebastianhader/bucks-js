0d7c767eaaf9a7333302e92c62314291a7917311
Sebastian Häde <sebastian@haede.com>
2022-07-17 09:51:37 +0200

    fix random max number

---
+++ __tests__/int.test.ts
@@ -0,0 +1,9 @@
+import $ from '../src/index'
+
+// INT
+
+test('$.int.random()', () => {
+    for (let i = 0; i < 20; i++) {
+        console.log($.int.random(1, 3))
+    }
+})
--- src/dataTypes/int.ts
+++ src/dataTypes/int.ts
@@ -18,18 +18,18 @@
     export const random = (min?: number, max?: number): number => {
         if (min && max) {
             min = Math.ceil(min)
-            max = Math.floor(max)
+            max = Math.floor(max + 1)
             // min and max set
             return Math.floor(Math.random() * Math.floor(max - min)) + min
         } else if (min && !max) {
             // only min is set
             return Math.floor(Math.random() * Math.floor(max)) + Math.ceil(min)
         } else if (!min && max) {
-            max = Math.floor(max)
+            max = Math.floor(max + 1)
             // only max is set
             return Math.floor(Math.random() * Math.floor(max))
         } else {
-            max = 100
+            max = 100000
             return Math.floor(Math.random() * max)
         }
     }
