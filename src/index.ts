e986340241d83193709111cf37cc694b1963c371
Sebastian Häde <sebastian@haede.com>
2022-11-23 18:45:33 +0100

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 import * as bucksJs from './bucks-js'
 export default bucksJs
+
+export * as string from './dataTypes/string.js'
+export * as int from './dataTypes/int.js'
+export * as array from './dataTypes/array.js'
+export * as object from './dataTypes/object.js'
+export { clone, urlQueryParam, debounce, sleep } from './bucks-js.js'
